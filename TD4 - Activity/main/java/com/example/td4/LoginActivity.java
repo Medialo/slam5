53788b7df829e08bb0133e6b8f9e188cca90cc76
Medialo <33297991+Medialo@users.noreply.github.com>
2020-10-11 15:30:06 +0200

    Add files via upload

--- TD4 - Activity/main/java/com/example/td4/LoginActivity.java
+++ TD4 - Activity/main/java/com/example/td4/LoginActivity.java
@@ -7,34 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class DetailActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_main);
         setTitle(getLocalClassName());
-
-        Button button = (Button) findViewById(R.id.btn_back_detail);
+        Button button = (Button) findViewById(R.id.btn_login);
 
         button.setOnClickListener(this);
-
-        NewsListApplication app = (NewsListApplication) getApplicationContext();
-
-        TextView textView = findViewById(R.id.textview111);
-        textView.setText(app.getLogin());
 
 
     }
 
-
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btn_back_detail:
+            case R.id.btn_login:
                 Intent i = new Intent(this, NewsActivity.class);
+                EditText editText = findViewById(R.id.edittext_login_login);
+                i.putExtra("login",editText.getText().toString());
+
+                NewsListApplication app = (NewsListApplication) getApplicationContext();
+                app.setLogin(editText.getText().toString());
+                System.out.println(app.getLogin());
                 startActivity(i);
                 break;
         }
---
+++ TD4 - Activity/main/java/com/example/td4/NewsListApplication.java
@@ -0,0 +1,26 @@
+package com.example.td4;
+
+import android.app.Application;
+
+public class NewsListApplication extends Application {
+
+
+    private String login;
+
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+
+}
